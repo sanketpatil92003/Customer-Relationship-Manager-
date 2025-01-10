67f965b2f1818bde1eb01546cdaa31d26f2bcec8
sanketpatil92003 <118122122+sanketpatil92003@users.noreply.github.com>
2025-01-10 23:13:17 +0530

    Update CustomerService.java

--- CustomerService.java
+++ CustomerService.java
@@ -1,1 +1,18 @@
+@Service
+public class CustomerService {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public void saveCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
